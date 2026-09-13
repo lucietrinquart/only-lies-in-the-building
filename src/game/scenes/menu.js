6e252ec88f4ec2d782d3f301f53d4f7ed944c813
lucietrinquart <118741664+lucietrinquart@users.noreply.github.com>
2026-09-13 14:49:28 +0200

    fix: image_public

--- src/game/scenes/menu.js
+++ src/game/scenes/menu.js
@@ -6,45 +6,45 @@ export default class menu extends Phaser.Scene {
     }
 
   preload() {
-    this.load.image("menu_fond", "../src/assets/menu_fond.png");
-    this.load.image("imageBoutonPlay", "../src/assets/button_play.png");
-    this.load.spritesheet("img_perso", "src/assets/dude.png", {
+    this.load.image("menu_fond", "/assets/menu_fond.png");
+    this.load.image("imageBoutonPlay", "/assets/button_play.png");
+    this.load.spritesheet("img_perso", "/assets/dude.png", {
         frameWidth: 40,
         frameHeight: 60
       });
 
       //gendarmerie
-      this.load.image("Phaser_tuilesdejeu1", "src/assets/sprite_police.png");
-    this.load.tilemapTiledJSON("carte1", "src/assets/map_police.json");
+      this.load.image("Phaser_tuilesdejeu1", "/assets/sprite_police.png");
+    this.load.tilemapTiledJSON("carte1", "/assets/map_police.json");
 
 
-    this.load.image("img_plateforme", "src/assets/platform.png");
-    this.load.spritesheet("img_perso", "src/assets/dude.png", {
+    this.load.image("img_plateforme", "/assets/platform.png");
+    this.load.spritesheet("img_perso", "/assets/dude.png", {
       frameWidth: 40,
       frameHeight: 60
     });
-    this.load.spritesheet("img_perso2", "src/assets/dude.png", {
+    this.load.spritesheet("img_perso2", "/assets/dude.png", {
       frameWidth: 40,
       frameHeight: 35
     });
-    this.load.spritesheet("img_perso3", "src/assets/dude.png", {
+    this.load.spritesheet("img_perso3", "/assets/dude.png", {
       frameWidth: 40,
       frameHeight: 60
     });
-    this.load.spritesheet("perso3", "src/assets/dude.png", {
+    this.load.spritesheet("perso3", "/assets/dude.png", {
       frameWidth: 40,
       frameHeight: 60
     });
-    this.load.spritesheet("perso4", "src/assets/dude.png", {
+    this.load.spritesheet("perso4", "/assets/dude.png", {
       frameWidth: 40,
       frameHeight: 60
     });
-    this.load.spritesheet("perso5", "src/assets/dude.png", {
+    this.load.spritesheet("perso5", "/assets/dude.png", {
       frameWidth: 40,
       frameHeight: 60
     });
 
-    this.load.image("img_porte1", "src/assets/door1.png");
+    this.load.image("img_porte1", "/assets/door1.png");
 
   }
 
